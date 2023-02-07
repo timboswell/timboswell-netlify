7cf66d12512286f563b4f6583a1a843f8ffdd0fd
Tim Boswell <timboswell@outlook.com>
2023-02-07 10:05:33 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
       <div className="container">
         <div className="navbar-brand">
           <Link to="/" className="navbar-item" title="Logo">
-            <img src={logo} alt="Tim Boswell" style={{ width: "88px" }} />
+            <img src={logo} alt="Tim Boswell" style={{ width: "110px" }} />
           </Link>
           {/* Hamburger menu */}
           <button
