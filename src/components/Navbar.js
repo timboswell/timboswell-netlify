94b4a2b1e43d8496c9f5b5e8bdd0de4cd63aa7d9
Tim Boswell <timboswell@outlook.com>
2023-02-07 10:26:45 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -48,11 +48,6 @@ const Navbar = () => {
               Contact
             </Link>
             </li>
-            <li className="navbar-item" style={{padding: "0px"}}>
-            <Link className="navbar-item" to="/contact/examples">
-              Form Examples
-            </Link>
-            </li>
           <li className="navbar-end has-text-centered">
             <a
               className="navbar-item"
